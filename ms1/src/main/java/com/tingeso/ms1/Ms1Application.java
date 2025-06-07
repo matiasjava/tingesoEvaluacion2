a98dbfa6bf88835f83280be28de6e330a6d6ca05
matiasjava <matias.vejar@usach.cl>
2025-06-07 00:02:49 -0400

    Se Realizan pequeños cambios de configuracion

--- ms1/src/main/java/com/tingeso/ms1/Ms1Application.java
+++ ms1/src/main/java/com/tingeso/ms1/Ms1Application.java
@@ -2,8 +2,10 @@ package com.tingeso.ms1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Ms1Application {
 
 	public static void main(String[] args) {
