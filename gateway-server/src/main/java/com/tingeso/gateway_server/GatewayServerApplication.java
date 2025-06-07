2aab732d9e76da3c7fb9dab30dc740f2595b76bb
matiasjava <matias.vejar@usach.cl>
2025-06-07 03:57:08 -0400

    Config

--- gateway-server/src/main/java/com/tingeso/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/tingeso/gateway_server/GatewayServerApplication.java
@@ -3,11 +3,8 @@ package com.tingeso.gateway_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
