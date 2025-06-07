144d71e7fad419eba452dbd04ca1a475040ea378
matiasjava <matias.vejar@usach.cl>
2025-06-07 18:09:58 -0400

    config

--- ms5/src/main/java/com/tingeso/ms5/Ms5Application.java
+++ ms5/src/main/java/com/tingeso/ms5/Ms5Application.java
@@ -2,8 +2,11 @@ package com.tingeso.ms5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Ms5Application {
 
 	public static void main(String[] args) {
